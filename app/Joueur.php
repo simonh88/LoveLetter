8797a5aad761b6d4812ce6edf4a8a10b279aa25e
Guillaume <guillaume.petit.etu@gmail.com>
2016-11-23 15:24:46 +0100

    bugfix

--- app/Joueur.php
+++ app/Joueur.php
@@ -196,10 +196,6 @@ class Joueur extends Model
         return Joueur::where('username', $username)->firstOrFail();
     }
 
-    public static function getJoueurByUsername($username) {
-        return Joueur::where('username', $username)->firstOrFail();
-    }
-
     public function deleteMain() {
         Main::where('joueur_id', $this->id)->delete();
     }
@@ -224,6 +220,7 @@ class Joueur extends Model
     public function ajouterPoint() {
         $this->nb_points = $this->nb_points + 1;
         $this->save();
+        Action::messageServeur($this->getSalon(), $this->username . " a gagné un point");
     }
 
     public function kingEffect($id_joueur_cible){
